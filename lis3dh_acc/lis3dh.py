56fb1cbf70d1487c31999c46d49426714ab7523a
Nishad Sabnis <nsabnis@ncsu.edu>
2016-11-18 18:54:11 -0500

    added simple test code

--- lis3dh_acc/lis3dh.py
+++ lis3dh_acc/lis3dh.py
@@ -10,16 +10,61 @@ from smbus import SMBus
 #changed
 LIS3DH = SMBus(1)
 
+def twosComp(x):
+    if(0x8000 & x):
+        x=-(0x010000 - x)
+    return x
+    
+def setDataRate(dataRate):
+    temp = LIS3DH.read_byte_data(LIS3DH_I2C_ADDR,CTRL_REG1)
+    temp &= 0b1111
+    temp |= (dataRate << 4)
+    LIS3DH.write_byte_data(LIS3DH_I2C_ADDR,CTRL_REG1)
+    
 def setup():
     #Check the WHO_AM_I address
     whoami = LIS3DH.read_byte_data(LIS3DH_I2C_ADDR,WHO_AM_I)
     if whoami != 0x33:
         print "LIS3DH with I2C address 0x18 not connected"
         sys.exit()
-    #Its connected so let us enable the XYZ axis
-    LIS3DH.write_byte_data(LIS3DH_I2C_ADDR,CTRL_REG1,0x77)
     
     #Set the data rate to 100HZ 
+    setDataRate(RATE_100HZ)
     
-def disconnect():
+    #Its connected so let us enable the XYZ axis
+    LIS3DH.write_byte_data(LIS3DH_I2C_ADDR,CTRL_REG1,0x57)
     
+def getValueX():
+    low_byte  = LIS3DH.read_byte_data(LIS3DH_I2C_ADDR,OUT_X_L)
+    high_byte = LIS3DH.read_byte_data(LIS3DH_I2C_ADDR,OUT_X_H)
+    result    = (high<<8) | low
+    result    = twosComp(result)
+    result    = float(result)/16380
+    return result
+    
+def getValueY():
+    low_byte  = LIS3DH.read_byte_data(LIS3DH_I2C_ADDR,OUT_Y_L)
+    high_byte = LIS3DH.read_byte_data(LIS3DH_I2C_ADDR,OUT_Y_H)
+    result    = (high<<8) | low
+    result    = twosComp(result)
+    result    = float(result)/16380
+    return result
+
+def getValueZ():
+    low_byte  = LIS3DH.read_byte_data(LIS3DH_I2C_ADDR,OUT_Z_L)
+    high_byte = LIS3DH.read_byte_data(LIS3DH_I2C_ADDR,OUT_Z_H)
+    result    = (high<<8) | low
+    result    = twosComp(result)
+    result    = float(result)/16380
+    return result
+
+def main():
+    setup()
+    while(True):
+        print getValueX()
+        print getValueY()
+        print getValueZ()
+        time.sleep(1)
+        
+if __name__=="__main__":
+    main()
